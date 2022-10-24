49c8797bd8829dcc7c4d3eed362ad8d978874523
Ezequiel Aleman <zekeale777@gmail.com>
2022-10-24 11:15:57 +1000

    Delete account functions

--- src/frontend/components/pages/Account.jsx
+++ src/frontend/components/pages/Account.jsx
@@ -14,53 +14,10 @@ const Root = styled('div')(({ theme }) => ({
   }));
 
 export const Account = () => {
-    const [blog_list_data, set_blog_list_data] = useState([]);
-
-    useEffect(() => {
-        fetch("/api/logins/identity")
-            .then((res) => res.json())
-            .then((response) => {
-              
-                    set_blog_list_data(response.body);
-               
-            });
-    }, []);
-
+   
     return (
         <Container>
             <h1>Blogs</h1>
-            <Box sx={{
-                display: 'flex',
-                flexDirection: 'column',
-              
-            }}>
-            {blog_list_data.map((blog_item_data) => (
-                <BlogItem blog={blog_item_data} />
-            ))}
-            </Box>
         </Container>
-    );
-};
-
-const BlogItem = ({ blog }) => {
-    return (
-       
-            <Root sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            
-        }}>
-            <Typography component="h1" sx={{
-                  color: "black",
-                  fontSize: "30px",
-                  fontFamily: 'Bebas Neue',
-            }}>{blog.blog_title ?? "Not specified"}</Typography>
-            <span>
-                {blog.blog_content ?? "Not specified"}
-            </span>
-            <span>{blog.blog_author ?? "Not specified"}</span>
-            <Divider sx={{mb: "20px",}}></Divider>
-            </Root>
-   
     );
 };
